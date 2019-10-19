464293c123e6dcda1d5242f79d65e5cfe04219c6
edlingao <sonido244@gmail.com>
2019-10-19 15:39:51 -0500

    feat(style):Improving style to be more spotify-like

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const electron = require("electron");
 const url = require("url");
 const path = require("path");
+
+// Enable live reload for all the files inside your project directory
+require('electron-reload')(__dirname);
+
 //Setup
 // main apis from electron
 
